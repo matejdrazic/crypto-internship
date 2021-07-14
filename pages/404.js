2f0597837afdf40766dcbc1489de2dd53f8f4f0d
matejdrazic <mdrazi01@fesb.hr>
2021-07-14 23:26:15 +0200

    fixing vercel issues #3

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,12 @@
 import Link from 'next/link'
-import Layout from '../components/Layout.js'
 import Image from 'next/image'
 
 const FourOFour = () => {
     return (
-        <Layout>
             <div className="not_found">
                 <Image src="/four.gif" alt="cartman" width="262px" height="200px" />
                 <h3>Go back to the <Link href="/"><a>login page.</a></Link></h3>
             </div>
-        </Layout>
     )
 }
 
