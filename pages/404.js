67407192e571e6c74dc219be1382fe16906bb171
matejdrazic <mdrazi01@fesb.hr>
2021-07-22 00:38:35 +0200

    Fixed path on 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Layout from '../components/layout.js'
+import Layout from '../components/Shared/Layout.js'
 import Image from 'next/image'
 
 const FourOFour = () => {
